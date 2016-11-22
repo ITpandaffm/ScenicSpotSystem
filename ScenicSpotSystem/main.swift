bc1d20e47bbf3c66fdb91fed9fedc32bcc49015f
ffm <fengfuming2007@126.com>
2016-11-22 12:02:35 +0800

    Initial Commit

---
+++ ScenicSpotSystem/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ScenicSpotSystem
+//
+//  Created by ffm on 16/11/22.
+//  Copyright © 2016年 ITPanda. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
